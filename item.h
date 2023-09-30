61a357b6467b1e6aa265b5b1912816d97773e1cf
Dsalo77 <83965923+Dsalo77@users.noreply.github.com>
2023-09-29 20:14:04 -0400

    Update item.h

--- item.h
+++ item.h
@@ -1,1 +1,33 @@
+#ifndef ITEM_H
+#define ITEM_H
 
+#include <string>
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class item{
+        public:
+                item();
+                //// Constructor to initialize the attributes
+                item(std::string name,std::string description, int calories, int weight);
+
+                //accessors
+                std::string getName();
+                std::string getDescription();
+                int getCalories();
+                int getWeight();
+
+                //setters
+                void setName(std::string name);
+                void setDescription(std::string description);
+                void setCalories(int calories);
+                void setWeight(int weight);
+
+        private:
+                std::string name;
+                std::string description;
+                int calories;
+                int weight;
+};
+#endif // ITEM_H
